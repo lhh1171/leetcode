5642d8a621deaa57b966f91a0534314be6ea1bcf
lhh1171 <2807652103@qq.com>
2022-06-14 20:51:01 +0800

    3

--- src/main/java/Solution3.java
+++ src/main/java/Solution3.java
@@ -2,10 +2,15 @@ class Solution3 {
     public int[] findDiagonalOrder(int[][] mat) {
         int row = 0, col = 0, row_max = mat.length, col_max = mat[0].length;
         int index = 0, direction = 1;
+
         int[][] choice = {{1, -1}, {-1, 1}};
         int[] ret = new int[row_max * col_max];
         while (row < row_max && col < col_max) {
+            //给ret赋值
             ret[index++] = mat[row][col];
+
+            //控制row和col来赋值ret
+            //z字打印direction控制方向
             if (direction == 1 && (row == 0 || col == col_max - 1)) {
                 direction = 0;
                 if (col < col_max - 1) {
@@ -23,6 +28,7 @@ class Solution3 {
             } else {
                 row += choice[direction][0];
                 col += choice[direction][1];
+                //对角线就是row-1,col+1或者row+1,col-1的走
             }
         }
         return ret;
