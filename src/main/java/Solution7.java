da9f168bb9d897ec549382b029d3408365d54dea
lhh1171 <2807652103@qq.com>
2022-06-30 22:01:18 +0800

    7

---
+++ src/main/java/Solution7.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution7 {
+    static int MOD = (int)1e9+7;
+    static List<Integer> list = new ArrayList<>();
+    static {
+        for (int i = 2; i <= 100; i++) {
+            boolean ok = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) ok = false;
+            }
+            if (ok) list.add(i);
+        }
+    }
+    public int numPrimeArrangements(int n) {
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (list.get(mid) <= n) l = mid;
+            else r = mid - 1;
+        }
+        int a = r + 1, b = n - a;
+        long ans = 1;
+        for (int i = b; i > 1; i--) ans = ans * i % MOD ;
+        for (int i = a; i > 1; i--) ans = ans * i % MOD ;
+        return (int)ans;
+    }
+}
+/*
+class demo{
+    public static void main(String[] args) {
+        Solution7 solution7=new Solution7();
+        System.out.println(solution7.numPrimeArrangements(3));
+
+    }
+}*/
