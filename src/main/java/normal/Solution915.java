ce34e66b64e317a2eb248e2c770fa6dc9d5469f0
lhh1171 <2807652103@qq.com>
2022-10-24 20:39:02 +0800

    915

--- src/main/java/normal/Solution915.java
+++ src/main/java/normal/Solution915.java
@@ -6,11 +6,10 @@ class Solution915 {
         int n = nums.length;
 
         int leftMax = nums[0], leftPos = 0, curMax = nums[0];
-
         for (int i = 1; i < n - 1; i++) {
             //比较curMax,nums[i]
             curMax = Math.max(curMax, nums[i]);
-            //当nums[i]小leftMax,leftMax需要更新为最大
+            //当nums[i]小leftMax,leftMax需要更新为当前遍历过的最大值curMax
             if (nums[i] < leftMax) {
                 leftMax = curMax;
                 leftPos = i;
