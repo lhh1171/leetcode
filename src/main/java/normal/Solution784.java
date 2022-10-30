125011a83828faa548149b89b8788a9eafa8739a
lhh1171 <2807652103@qq.com>
2022-10-30 21:23:52 +0800

    784

---
+++ src/main/java/normal/Solution784.java
@@ -0,0 +1,28 @@
+package normal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution784 {
+
+    public List<String> letterCasePermutation(String S) {
+        List<String> res = new ArrayList<>();
+        char[] charArray = S.toCharArray();
+        dfs(charArray, 0, res);
+        return res;
+    }
+
+    private void dfs(char[] charArray, int index, List<String> res) {
+        if (index == charArray.length) {
+            res.add(new String(charArray));
+            return;
+        }
+
+        dfs(charArray, index + 1, res);
+        if (Character.isLetter(charArray[index])) {
+            charArray[index] ^= 1 << 5;
+            dfs(charArray, index + 1, res);
+        }
+    }
+}
+
