4aaad1e52dbe023eb451f14a11db65893e806dfe
lhh1171 <2807652103@qq.com>
2022-12-08 08:48:47 +0800

    hot100

---
+++ src/main/java/hot100/Solution1.java
@@ -0,0 +1,18 @@
+package hot100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//查找元素题目
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
