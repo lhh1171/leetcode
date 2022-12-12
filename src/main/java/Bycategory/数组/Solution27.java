db7a9706e176987db11439e76c1403b5b5801071
lhh1171 <2807652103@qq.com>
2022-12-12 18:15:03 +0800

    by category

---
+++ src/main/java/Bycategory/数组/Solution27.java
@@ -0,0 +1,46 @@
+package Bycategory.数组;
+
+public class Solution27 {
+
+    public int removeElement1(int[] nums, int val) {
+        // 快慢指针
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex] = nums[fastIndex];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
+    }
+
+    //相向双指针法
+    public int removeElement2(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        //将right移到从右数第一个值不为val的位置
+        while (right >= 0 && nums[right] == val) right--;
+        while (left <= right) {
+            //left位置的元素需要移除
+            if (nums[left] == val) {
+                //将right位置的元素移到left（覆盖），right位置移除
+                nums[left] = nums[right];
+                right--;
+            }
+            left++;
+            //将right移到一个值不为val的位置
+            while (right >= 0 && nums[right] == val) right--;
+        }
+        return left;
+    }
+
+
+    public static void main(String[] args) {
+        //两个指针，一个指针计算除了目标数组之后的长度，一个指针负责遍历偏移
+        //fast slow
+        //right left
+        Solution27 solution27 = new Solution27();
+        System.out.println(solution27.removeElement1(new int[]{1, 2, 3, 4, 3}, 3));
+    }
+
+}
---
+++ src/main/java/Bycategory/数组/Solution704.java
@@ -0,0 +1,26 @@
+package Bycategory.数组;
+
+public class Solution704 {
+    public int findMiddle(int[] nums, int target){
+        int left=0;
+        int right=nums.length-1;
+        int middle=0;
+        while (left<=right){
+            middle=(left+right)/2;
+            if (nums[middle]>target){
+                //当前偏左的位置也不满足
+                right=middle-1;
+            }else if (nums[middle]<target){
+                left=left+1;
+            }else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution704 solution704=new Solution704();
+        System.out.println(solution704.findMiddle(new int[]{1, 2, 3, 4}, 3));
+    }
+}
