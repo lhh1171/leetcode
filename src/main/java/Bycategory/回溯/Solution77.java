7c25b74b13dd92736474b36db0267b52ed6b63f1
lhh1171 <2807652103@qq.com>
2023-01-02 22:55:07 +0800

    by category

--- src/main/java/Bycategory/回溯/Solution77.java
+++ src/main/java/Bycategory/回溯/Solution77.java
@@ -9,8 +9,8 @@ public class Solution77 {
     List<List<Integer>> result = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
 
-    public List<List<Integer>> combine(int n, int k) {
-        combineHelper(n, k, 1);
+    public List<List<Integer>> combine1(int n, int k) {
+        combineHelper1(n, k, 1);
         return result;
     }
 
@@ -19,7 +19,7 @@ public class Solution77 {
      *
      * @param startIndex 用来记录本层递归的中，集合从哪里开始遍历（集合就是[1,...,n] ）。
      */
-    private void combineHelper(int n, int k, int startIndex) {
+    private void combineHelper1(int n, int k, int startIndex) {
         //终止条件
         if (path.size() == k) {
             result.add(new ArrayList<>(path));
@@ -28,9 +28,33 @@ public class Solution77 {
         for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) {
             path.add(i);
             //递归
-            combineHelper(n, k, i + 1);
+            combineHelper1(n, k, i + 1);
             path.removeLast();
         }
     }
+
+
+    public List<List<Integer>> combine2(int n, int k) {
+        combineHelper2(n, k, 1);
+        return result;
+    }
+
+    /**
+     * 每次从集合中选取元素，可选择的范围随着选择的进行而收缩，调整可选择的范围，就是要靠startIndex
+     * @param startIndex 用来记录本层递归的中，集合从哪里开始遍历（集合就是[1,...,n] ）。
+     */
+    private void combineHelper2(int n, int k, int startIndex){
+        //终止条件
+        if (path.size() == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++){
+            path.add(i);
+            combineHelper2(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+
 
 }
