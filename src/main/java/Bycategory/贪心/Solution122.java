7dc4485138db0da71d4716ee8cd8a4e7433fbf3d
lhh1171 <2807652103@qq.com>
2023-01-21 23:48:51 +0800

    by category

---
+++ src/main/java/Bycategory/贪心/Solution122.java
@@ -0,0 +1,12 @@
+package Bycategory.贪心;
+
+// 贪心思路
+class Solution122 {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        for (int i = 1; i < prices.length; i++) {
+            result += Math.max(prices[i] - prices[i - 1], 0);
+        }
+        return result;
+    }
+}
