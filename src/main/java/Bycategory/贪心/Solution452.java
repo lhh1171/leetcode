f9e92d48274a956bea7ec4e91e7932715cd3a562
lhh1171 <2807652103@qq.com>
2023-01-26 22:55:50 +0800

    by category

---
+++ src/main/java/Bycategory/贪心/Solution452.java
@@ -0,0 +1,25 @@
+package Bycategory.贪心;
+
+import java.util.Arrays;
+
+/**
+ * 时间复杂度 : O(NlogN)  排序需要 O(NlogN) 的复杂度
+ * 空间复杂度 : O(logN) java所使用的内置函数用的是快速排序需要 logN 的空间
+ */
+class Solution452 {
+    public int findMinArrowShots(int[][] points) {
+        // 根据气球直径的开始坐标从小到大排序
+        // 使用Integer内置比较方法，不会溢出
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int count = 1;  // points 不为空至少需要一支箭
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > points[i - 1][1]) {  // 气球i和气球i-1不挨着，注意这里不是>=
+                count++; // 需要一支箭
+            } else {  // 气球i和气球i-1挨着
+                points[i][1] = Math.min(points[i][1], points[i - 1][1]); // 更新重叠气球最小右边界
+            }
+        }
+        return count;
+    }
+}
