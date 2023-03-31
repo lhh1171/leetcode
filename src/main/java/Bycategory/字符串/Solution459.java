ccc3d6d9743fd87ef7fa4e5fe5bf884cd6b47400
lhh1171 <2807652103@qq.com>
2023-03-31 20:46:13 +0800

    xx

--- src/main/java/Bycategory/字符串/Solution459.java
+++ src/main/java/Bycategory/字符串/Solution459.java
@@ -12,16 +12,29 @@ public class Solution459 {
 
         // 构造 next 数组过程，j从0开始(空格)，i从2开始
         for (int i = 2, j = 0; i <= len; i++) {
+            //从后往前匹配，
             // 匹配不成功，j回到前一位置 next 数组所对应的值
-            while (j > 0 && chars[i] != chars[j + 1]) j = next[j];
-            // 匹配成功，j往后移
+            while (j > 0 && chars[i] != chars[j + 1]){
+                System.out.println("----------");
+                j = next[j];
+            }
+            // 匹配成功，j往后移,chars[i] 和 chars[j+1]
             if (chars[i] == chars[j + 1]) j++;
+            System.out.println(s.substring(0, i));
+            System.out.println(s.substring(0, j));
+            System.out.println(i+"..........."+j);
             // 更新 next 数组的值
             next[i] = j;
         }
-
+        for (int i:
+             next) {
+            System.out.print(i +" ");
+        }
+        System.out.println();
         // 最后判断是否是重复的子字符串，这里 next[len] 即代表next数组末尾的值
         // len % (len - next[len])表示len%（(字符串的长度) - (最长公共前后缀的长度) ）-----------是否能整除
+        //len - next[len]------(字符串的长度) - (最长公共前后缀的长度) ---------表示最小子串单元的长度
+        System.out.println(len+","+next[len]);
         if (next[len] > 0 && len % (len - next[len]) == 0) {
             return true;
         }
@@ -42,11 +55,20 @@ public class Solution459 {
         //ab的最长相等前后缀是0，因为他他前缀是a,后缀是b
         //aba  a  a 1
         //abab ab ab 2
-        //ababa aba aba 3
+        //ababa aba aba 3------>ababf 从后往前匹配 a ab aba abab:b ba bab babf没有一个匹配的
         //ababab abab abab 4
         //abababa ababa ababa 5
         //abababab ababab ababab 6
         //0 0 1 2 3 4 5 6
-        solution459.repeatedSubstringPattern("abababab");
+
+
+        //一般使用双指针法，在当前子串，i表示后缀末尾，j表示前缀的末尾（代表最长相等前后缀的长度）
+//        System.out.println(solution459.repeatedSubstringPattern("abcabcabcabc"));
+
+        System.out.println(solution459.repeatedSubstringPattern("ababfbab"));
+
+//        补充一下
+        System.out.println(solution459.repeatedSubstringPattern("ababfbababfb"));
+
     }
 }
